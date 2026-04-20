37459b9e34ec1ae33863978279961cbd5bc1b7e2
oyin <oyinlolu@gmail.com>
2026-04-20 01:04:23 +0100

    add get account api

--- api/account.go
+++ api/account.go
@@ -1,15 +1,16 @@
 package api
 
 import (
-// 	"fmt"
-    "net/http"
+	// 	"fmt"
+	"database/sql"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	// "github.com/gin-gonic/gin/binding"
 	// "github.com/go-playground/validator/v10"
 	db "github.com/Oyinoye/bank_mini/db/sqlc"
-// 	"github.com/Oyinoye/bank_mini/token"
-// 	"github.com/Oyinoye/bank_mini/util"
+	// "github.com/Oyinoye/bank_mini/token"
+	// "github.com/Oyinoye/bank_mini/util"
 )
 
 type createAccountRequest struct {
@@ -44,3 +45,37 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, account)
 }
+
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	// if account.Owner != authPayload.Username {
+	// 	err := errors.New("account doesn't belong to the authenticated user")
+	// 	ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+	// 	return
+	// }
+
+	ctx.JSON(http.StatusOK, account)
+}
+
